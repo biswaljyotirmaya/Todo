f151adb10a11073ddec9cee3dc4f337ca88184d3
biswaljyotirmaya <biswaljyotirmaya06@gmail.com>
2024-01-23 09:34:27 +0530

    Updated features

--- src/TodoList/todo.jsx
+++ src/TodoList/todo.jsx
@@ -1,52 +1,124 @@
 import React, { useState } from 'react';
-import { RxCross2 } from "react-icons/rx";
+import { MdEdit } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
+import Header from './Header';
 
+const todo = () => {
+  const [itemsList, setItemsList] = useState([]);
+  const [item, setItem] = useState("");
+  const [editingItem, setEditingItem] = useState(false); 
+  const [statuses, setStatuses] = useState({});
+  const [updated, setUpdated] = useState("");
 
-const Todo = () => {
-    const [itemsList, setItemsList] = useState(['Thing 1', 'Thing 2']);
-    const [item, setItem] = useState("");
-    const addItem = () => {
-        setItemsList(prevState => [...prevState, item]);
-        setItem("");
-    };
-    const deleteItem = (item) => {
-        setItemsList(prevState => prevState.filter(existingItem => existingItem !== item));
-    };
-    const list = itemsList.map(item => (
-        <p key={item} className='text-white text-xl mb-2 font-semibold flex justify-between'>
-            {item}<RxCross2 className='mr-6 hover:h-6 w-6 cursor-pointer' onClick={() => deleteItem(item)}/>
-        </p>
-    ));
+  const addItem = () => {
+    setItemsList((prevState) => [...prevState, item]);
+    setStatuses((prevStatuses) => ({ ...prevStatuses, [item]: 'border px-2 py-1 border-gray-400 rounded text-gray-400 cursor-pointer' }));
+    setItem("");
+  };
 
-    return (
-        <div className='grid place-items-center  h-[100vh] bg-black'>
-        <div className='bg-blue-600 w-96 p-4 rounded-xl'>
-            <h3 className='text-4xl font-bold text-white text-center mt-4'>TO DO LIST</h3>
-            <p className='text-center text-white font-bold mb-4'>It will help you to remember things</p>
-            <input
-                type="text"
-                className='w-80 h-10 ml-4 placeholder:text-center rounded focus:outline-none focus:caret-pink-500 caret:ml-4 pl-3'
-                placeholder='Enter task to add in the list'
-                value={item}
-                onChange={(e) => {
-                    setItem(e.target.value);
-                }}
-            />
-            
-    {item && <button
-    className='text-white bg-black rounded-lg text-xl mt-4 text-center w-full h-8 font-semibold'
-    onClick={item ? addItem : undefined}>
-    Add item into list
-     </button>}
-          
+  const deleteItem = (item) => {
+    setItemsList((prevState) => prevState.filter((existingItem) => existingItem !== item));
+    setStatuses((prevStatuses) => {
+      const { [item]: _, ...rest } = prevStatuses;
+      return rest;
+    });
+  };
 
-            <hr className='bg-white mt-6' />
-            {list.length !=0 ? <p className='text-white font-bold text-2xl mt-4 text-center'>Task to do</p> : <p className='text-white font-bold text-xl mt-4 text-center'>You don't have any task to do</p>}
-            <div className='mt-2'>{list}</div>
-        </div>
-        </div>
+  const editItem = (item) => {
+    setEditingItem(item);
+  };
+
+  const saveUpdatedItem = () => {
+    setItemsList((prevState) =>
+      prevState.map((existingItem) => (existingItem === editingItem ? updatedItem : existingItem))
     );
-};
+    setEditingItem(null);
+  };
+ const itemUpdate = () =>{
+  setEditingItem(false);
+ }
 
-export default Todo;
+  const statusCheck = (item) => {
+    setStatuses((prevStatuses) => ({ ...prevStatuses, [item]: 'border px-2 py-1 border-green-400 rounded text-green-400 cursor-pointer' }));
+  };
+
+  const list = itemsList.map((item) => (
+    <div className="w-full text-[0.8rem]" key={item}>
+      <div className="mb flex justify-around items-center">
+        <div className="w-1/12 text-center">{itemsList.indexOf(item) + 1}</div>
+        {editingItem ? <div className='w-7/12 flex'>
+          <input type="text" value={{item}} className='w-4/5 text-center focus:outline-none' />
+          <div className=' border rounded text-blue-400 border-blue-400 text-center w-1/5 py-1 cursor-pointer' onClick={itemUpdate}>Add Task</div></div>:
+        <div className='w-7/12 text-center'>{item}</div>}
+        <div className="w-2/12 text-center ">
+          <div className="flex justify-center p-1 ">
+            <div className={statuses[item]} onClick={() => statusCheck(item)}>
+              Todo
+            </div>
+          </div>
+        </div>
+        <div className="w-1/12 text-center">
+          <div className="flex justify-center p-1 ">
+            {editingItem === item ? (
+              <button
+                className="border p-1 border-blue-400 rounded-sm text-blue-400 cursor-pointer"
+                onClick={saveUpdatedItem}
+              >
+                <MdEdit className="h-5 w-5" />
+              </button>
+            ) : (
+              <button
+                className="border p-1 border-blue-400 rounded-sm text-blue-400 cursor-pointer"
+                onClick={() => editItem(item)}
+              >
+                <MdEdit className="h-5 w-5" />
+              </button>
+            )}
+          </div>
+        </div>
+        <div className="w-1/12 text-center">
+          <div className="flex justify-center p-1 ">
+            <div
+              className="border p-1 border-gray-400 rounded-sm text-gray-400 cursor-pointer"
+              onClick={() => deleteItem(item)}
+            >
+              <MdDelete className="h-5 w-5" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr />
+    </div>
+  ));
+
+  return (
+    <div className="grid place-items-center  h-[100vh] bg-black">
+      <div className="bg-white w-[822px] p-14">
+        <h3 className="text-4xl font-bold text-black mb-2">TODO List App</h3>
+        <span className=" text-black my bg-[#FDF8E4] rounded">Do it now.</span>
+        <br />
+        <div className="flex justify-around my-4">
+          <input
+            type="text"
+            className="w-80 h-10 ml-4 placeholder:text-center rounded focus:outline-none focus:caret-pink-500 pl-3 placeholder:text-xl"
+            placeholder="Enter task to add in the list"
+            value={item}
+            onChange={(e) => {
+              setItem(e.target.value);
+            }}
+          />
+          {item && (
+            <button
+              className="border rounded items-center text-sky-400 text-sm border-sky-400 px-2 py-1"
+              onClick={item ? addItem : undefined}
+            >
+              Add Task
+            </button>
+          )}
+        </div>
+
+      <Header list={list}/>
+      </div>
+    </div>
+)}
+export default todo
